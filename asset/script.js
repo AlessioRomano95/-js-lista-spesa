7a6440c26b27fd770bb66123e953ca882c966e33
AlessioRomano95 <romanalex400@gmail.com>
2024-02-29 19:38:32 +0100

    [Add] const iniziale

---
+++ asset/script.js
@@ -0,0 +1,23 @@
+const listaSpesa = [
+  "Pomodori",
+  "Cipolle",
+  "Basilico",
+  "Pepe Nero",
+  "Olio d'oliva",
+  "Cassa d'acqua",
+  "Nutella",
+  "Uova",
+  "Latte",
+  "Farina",
+  "Pasta",
+  "Carne",
+  "Pollo",
+  "Prosciutto crudo",
+  "Patate",
+  "Broccoli",
+  "Guanciale",
+  "Pecorino",
+  "Peperoncino",
+];
+console.log(listaspesa)
+
